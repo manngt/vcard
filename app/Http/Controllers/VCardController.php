340cde6860d6d74e0e58f438d16ed7b0e6706ce1
MAUS502 <manngt@gmail.com>
2018-02-11 19:22:58 -0600

    vcard

--- app/Http/Controllers/VCardController.php
+++ app/Http/Controllers/VCardController.php
@@ -37,10 +37,10 @@ class VCardController extends Controller
 //return $vcard->getOutput();
 
 // return vcard as a download
-        return $vcard->download();
+        //return $vcard->download();
 
 // save vcard on disk
-//$vcard->setSavePath('/path/to/directory');
+return $vcard->setSavePath('/');
 //$vcard->save();
     }
 }
